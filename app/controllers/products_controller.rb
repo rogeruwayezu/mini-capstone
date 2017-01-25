9be34a8bfd73104dacba897c24991c566d7ff792
rogeruwayezu <rogeruwayezu@gmail.com>
2017-01-25 22:12:41 +0200

    used REST

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,44 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.html.erb"
+  end
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+  def new
+
+  end
+  def create
+    name = params[:name]
+    description = params[:description]
+    price = params[:price]
+    image = params[:image]
+    product = Product.new({name: name, description: description, price: price, image: image})
+    product.save
+    flash[:info] = "Product created"
+    redirect_to "/products/#{product.id}"
+  end
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+  def update
+    product = Product.find_by(id: params[:id])
+    product.assign_attributes({})
+    product.name = params[:name]
+    product.description = params[:description]
+    product.price = params[:price]
+    product.image = params[:image]
+    
+    product.save
+    flash[:success] = "Product updated"
+    redirect_to "/products/#{product.id}" 
+  end
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    flash[:warning] = "Product deleted"
+    redirect_to "/products"
+  end
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  def tax
+    price * 0.09
+  end
+  def friendly_created_at
+    created_at_strftime("%B %e, %Y")
+  end
+  def price_list
+    price.split(", ")
+  end
 end
