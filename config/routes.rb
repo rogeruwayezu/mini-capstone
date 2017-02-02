ebb489cdff477cc2920601f54de8a75c1a07a1d9
rogeruwayezu <rogeruwayezu@gmail.com>
2017-02-02 09:01:53 +0200

    added association for images and products tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: 'products#index'
    get "/products", to: 'products#index'
    get "/products/new", to:'products#new'
    get "/products/:id", to: 'products#show'
