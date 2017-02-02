6e834ad87a99df0a484c1397606314b4e99680e4
rogeruwayezu <rogeruwayezu@gmail.com>
2017-02-02 18:48:33 +0200

    Removed image attribute in Product and showed more than one picture of the product

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
    get "/products/:id/edit", to: 'products#edit'
    patch "/products/:id", to: 'products#update'
    delete "/products/:id", to: 'products#destroy'
+   get "/signup" => "users#new"
+   post "/users" => "users#create"
+   get "/login" => "session#new"
+   post "/login" => "sessions#create"
+   delete "logout" => "sessions#destroy"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131133839) do
+ActiveRecord::Schema.define(version: 20170202145439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20170131133839) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price",       precision: 7, scale: 2
-    t.string   "image"
     t.text     "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
@@ -40,6 +39,14 @@ ActiveRecord::Schema.define(version: 20170131133839) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
